6c091ef435733ce6b7c416191f409de7868bfbd4
4aykovski <kan89kan@gmail.com>
2024-05-27 21:49:30 +0700

    my nvim config

---
+++ mappings.lua
@@ -0,0 +1,75 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    },
+    ["<leader>dc"] = {
+      function ()
+        require('dap').continue()
+      end,
+      "Debug start session"
+    },
+    ["<F10>"] = {
+      function ()
+        require('dap').step_over()
+      end
+    },
+    ["<F11>"] = {
+      function ()
+        require('dap').step_into()
+      end
+    },
+    ["<F12>"] = {
+      function ()
+        require('dap').step_out()
+      end
+    },
+  }
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require('dap-go').debug_test()
+      end,
+      "Debug go test"
+    },
+    ["<leader>dgl"] = {
+      function()
+        require('dap-go').debug_last()
+      end,
+      "Debug last go test"
+    }
+  }
+}
+
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json struct tags"
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags"
+    }
+  }
+}
+
+return M
+
---
+++ plugins.lua
@@ -0,0 +1,75 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
+  {
+    "nvim-neotest/nvim-nio"
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      local dap = require('dap')
+      local dapui = require('dapui')
+      dapui.setup(opts)
+       dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+       end
+       dap.listeners.before.launch.dapui_config = function()
+         dapui.open()
+       end
+       dap.listeners.before.event_terminated.dapui_config = function()
+         dapui.close()
+       end
+       dap.listeners.before.event_exited.dapui_config = function()
+          dapui.close()
+       end
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function (_, opts)
+    end
+  }
+}
+return plugins
