4894721ae1df9caca37c43dc744a300c1dd9aec7
4aykovski <kan89kan@gmail.com>
2024-06-08 08:28:27 +0300

    changes

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -21,3 +21,68 @@ lspconfig.gopls.setup {
   },
 }
 
+lspconfig.jsonls.setup {
+ cmd = { 'vscode-json-language-server', '--stdio' },
+    filetypes = { 'json', 'jsonc' },
+    init_options = {
+      provideFormatter = true,
+    },
+    root_dir = util.find_git_ancestor,
+    single_file_support = true,
+}
+
+lspconfig.cssls.setup {
+    cmd = { 'vscode-css-language-server', '--stdio' },
+    filetypes = { 'css', 'scss', 'less' },
+    init_options = { provideFormatter = true }, -- needed to enable formatting capabilities
+    root_dir = util.root_pattern('package.json', '.git'),
+    single_file_support = true,
+    settings = {
+      css = { validate = true },
+      scss = { validate = true },
+      less = { validate = true },
+    },
+}
+
+lspconfig.html.setup {
+   cmd = { 'vscode-html-language-server', '--stdio' },
+    filetypes = { 'html', 'templ' },
+    root_dir = util.root_pattern('package.json', '.git'),
+    single_file_support = true,
+    settings = {},
+    init_options = {
+      provideFormatter = true,
+      embeddedLanguages = { css = true, javascript = true },
+      configurationSection = { 'html', 'css', 'javascript' },
+    },
+}
+
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+lspconfig.tsserver.setup {
+    init_options = { hostInfo = 'neovim' },
+    cmd = { 'typescript-language-server', '--stdio' },
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'javascript.jsx',
+      'typescript',
+      'typescriptreact',
+      'typescript.tsx',
+    },
+    commands = {
+      OrganizeImports = {
+        organize_imports,
+        description = 'Organize Imports',
+      },
+    },
+    root_dir = util.root_pattern('tsconfig.json', 'jsconfig.json', 'package.json', '.git'),
+    single_file_support = true,
+}
+
--- plugins.lua
+++ plugins.lua
@@ -3,45 +3,52 @@ local plugins = {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "eslint-lsp",
+        "prettier",
         "gopls",
+        "json-lsp",
+        "css-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "stylua",
       },
     },
   },
   {
-    'Exafunction/codeium.vim',
-    event = 'BufEnter'
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
   },
   {
-   'Wansmer/treesj',
+    "Wansmer/treesj",
     keys = {
-        {
-         "gJ",
-         function()
+      {
+        "gJ",
+        function()
           require("treesj").join()
-         end,
-         desc = "Join",
-        },
-        {
-         "gS",
-         function()
-          require("treesj").split()
-         end,
-         desc = "Split",
-        },
-        {
-          "gM",
-          function()
-            require("treesj").toggle()
-          end,
-          desc = "Toggle",
-        }
+        end,
+        desc = "Join",
       },
-    dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+      {
+        "gS",
+        function()
+          require("treesj").split()
+        end,
+        desc = "Split",
+      },
+      {
+        "gM",
+        function()
+          require("treesj").toggle()
+        end,
+        desc = "Toggle",
+      },
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
     config = function()
-      require('custom.configs.treesj')
+      require "custom.configs.treesj"
     end,
   },
-   {
+  {
     "folke/trouble.nvim",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
@@ -79,47 +86,47 @@ local plugins = {
     },
   },
   {
-      "smoka7/multicursors.nvim",
-      event = "VeryLazy",
-      dependencies = {
-          'smoka7/hydra.nvim',
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "smoka7/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
       },
-      opts = {},
-      cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
-      keys = {
-              {
-                  mode = { 'v', 'n' },
-                  '<Leader>m',
-                  '<cmd>MCstart<cr>',
-                  desc = 'Create a selection for selected text or word under the cursor',
-              },
-          },
+    },
   },
   {
-    "nvim-neotest/nvim-nio"
+    "nvim-neotest/nvim-nio",
   },
   {
     "mfussenegger/nvim-dap",
     dependencies = "mfussenegger/nvim-dap",
     config = function(_, opts)
-      local dap = require('dap')
-      local dapui = require('dapui')
+      local dap = require "dap"
+      local dapui = require "dapui"
       dapui.setup(opts)
-       dap.listeners.before.attach.dapui_config = function()
+      dap.listeners.before.attach.dapui_config = function()
         dapui.open()
-       end
-       dap.listeners.before.launch.dapui_config = function()
-         dapui.open()
-       end
-       dap.listeners.before.event_terminated.dapui_config = function()
-         dapui.close()
-       end
-       dap.listeners.before.event_exited.dapui_config = function()
-          dapui.close()
-       end
-      require("core.utils").load_mappings("dap")
-      require("core.utils").load_mappings("dapui")
-    end
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+      require("core.utils").load_mappings "dap"
+      require("core.utils").load_mappings "dapui"
+    end,
   },
   {
     "leoluz/nvim-dap-go",
@@ -127,8 +134,8 @@ local plugins = {
     dependencies = "mfussenegger/nvim-dap",
     config = function(_, opts)
       require("dap-go").setup(opts)
-      require("core.utils").load_mappings("dap_go")
-    end
+      require("core.utils").load_mappings "dap_go"
+    end,
   },
   {
     "neovim/nvim-lspconfig",
@@ -139,17 +146,29 @@ local plugins = {
   },
   {
     "jose-elias-alvarez/null-ls.nvim",
-    ft = "go",
-    opts = function()
+    event = "VeryLazy",
+    opts = function ()
       return require "custom.configs.null-ls"
-    end,
+    end
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    lazy = true,
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("nvim-ts-autotag").setup(opts)
+    end
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = true
   },
   {
     "olexsmir/gopher.nvim",
     ft = "go",
     config = function(_, opts)
       require("gopher").setup(opts)
-      require("core.utils").load_mappings("gopher")
+      require("core.utils").load_mappings "gopher"
     end,
     build = function()
       vim.cmd [[silent! GoInstallDeps]]
@@ -157,6 +176,6 @@ local plugins = {
   },
   {
     "rcarriga/nvim-dap-ui",
-  }
+  },
 }
 return plugins
